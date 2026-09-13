634d9e7890f528df9d27c6d8e6f46904bb8b67e5
FerdouseOfficial <bsource@gmail.com>
2026-09-13 20:06:25 +0600

    We have practice the math function and everything is clear I think

--- MathFunctions.c
+++ MathFunctions.c
@@ -3,11 +3,21 @@
 
 int main() {
 
-    int x = 9;
+    float x = 3;
 
-    x = sqrt(x);
+    // x = sqrt(x);
+    // x = pow(x, 2);
+    // x = round(x); If the point value is above 500 it will make it 10 I mean add one  there  ;
+    // x = ceil(x); No matter what the value it will make it 10 I mean add 1 to the value and make it a round figure;
+    // x = floor(x); No matter what the value it will make all the point number useless it will only show the actual integer number only 
+    // x = abs(x); 
+    // x = log(x);
+    // x = sin(x);
+    // x = cos(x);
+    // x = tan(x);
 
-    printf("%d", x);
+
+    printf("%f", x);
 
     return 0;
 }
