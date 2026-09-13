b099f69f699be636b8e102500898e6809da440ca
FerdouseOfficial <bsource@gmail.com>
2026-09-13 20:14:00 +0600

    I have started solving the problem of the Area. Here what we do is at first we will write the syntax then declare the variable that will store the data and we will get the input and then writing the logic that will get our work done and after that run the code take input and give the user his output okay

---
+++ questions/Area.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    double radius = 0.0;
+    double area = 0.0;
+    const double PI = 3.14159;
+
+    printf("Enter the radius: ");
+    scanf("%lf", &radius);
+
+
+    return 0;
+}
