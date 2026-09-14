45429af1ea5caca6a54eab8e04d6e885a2097604
FerdouseOfficial <bsource@gmail.com>
2026-09-14 09:24:11 +0600

    facing some issues let me handle this

--- Projects/WeighCoverterProgram.c
+++ Projects/WeighCoverterProgram.c
@@ -7,7 +7,7 @@ int main() {
     float pounds = 0.0f;
     float kilograms = 0.0f;
 
-    printf("Weight Conversion Calculator");
+    printf("Weight Conversion Calculator\n");
     printf("1. Kilograms to Pounds\n");
     printf("2. Pounds to Kilograms");
     printf("Enter Your choice (1/2): "); 
@@ -15,10 +15,19 @@ int main() {
     if (choice == 1)
     {
         // Kilograms to Pounds
+        scanf("%f", &choice);
         printf("Enter the weight in Kilograms");
-        scanf("%f", &kilograms);
         pounds = kilograms * 2.20462;
         printf("%.2f kilograms is equal to %.2f Pounds", kilograms, pounds);
+    }
+    else if(choice == 2) {
+        printf("Enter the widht in pounds: ");
+        scanf("%f", &pounds);
+        kilograms = pounds * 2.20234;
+        printf("%.2f Pound is equal to %.2f Kilograms", pounds, kilograms);
+    }
+    else{
+        printf('Invalid choice! Please enter 1 or 2\n');
     }
     
 
