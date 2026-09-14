358ee6a67902de3970d85ad562e2d41bd17950c1
FerdouseOfficial <bsource@gmail.com>
2026-09-14 09:13:30 +0600

    Yeah 50% of the project work is done from now on

---
+++ Projects/WeighCoverterProgram.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    // WEIGHT CONVERTER PROGRAM
+    int choice = 0;
+    float pounds = 0.0f;
+    float kilograms = 0.0f;
+
+    printf("Weight Conversion Calculator");
+    printf("1. Kilograms to Pounds\n");
+    printf("2. Pounds to Kilograms");
+    printf("Enter Your choice (1/2): "); 
+
+    return 0;
+}
