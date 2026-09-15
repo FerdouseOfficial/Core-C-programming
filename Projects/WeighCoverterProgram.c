39842128c3f9ba9f8978542824a90e5ba9ac3ac0
FerdouseOfficial <bsource@gmail.com>
2026-09-16 04:29:43 +0600

    The weight convertere project is done perfectly see I can handle this, I can do this all day

--- Projects/WeighCoverterProgram.c
+++ Projects/WeighCoverterProgram.c
@@ -15,12 +15,18 @@ int main() {
     // Conditions 
     if(choice == 1) {
         // Kilograms to Pounds
-        printf("Enter the weight in kilograms");
+        printf("Enter the weight in kilograms: ");
+        scanf("%f", &kilograms);
+        pounds = kilograms * 2.20462;
+        printf("%.2f kilograms is equals to %.2f Pounds\n", kilograms, pounds);
     } 
     else if (choice == 2)
     {
         // Pounds to Kilograms
-        printf("Enter the weight in Pounds");
+        printf("Enter the weight in Pounds: ");
+        scanf("%f", &pounds);
+        kilograms = pounds / 2.20462;
+        printf("%.2f pounds is equal to %.2f kilograms\n", pounds, kilograms);
     }
     else {
         printf("Invalid Choice! Please enter 1 or 2\n");
