b46bd97dbfd7c69a6eaed69a3afe7d4ae7011d63
FerdouseOfficial <bsource@gmail.com>
2026-09-16 04:22:23 +0600

    The formula is a bit tricky but It won't get me let me handle this

--- Projects/WeighCoverterProgram.c
+++ Projects/WeighCoverterProgram.c
@@ -2,33 +2,29 @@
 
 int main() {
 
-    // WEIGHT CONVERTER PROGRAM
     int choice = 0;
     float pounds = 0.0f;
     float kilograms = 0.0f;
 
-    printf("Weight Conversion Calculator\n");
+    printf("Weight Converstion Calculator\n");
     printf("1. Kilograms to Pounds\n");
-    printf("2. Pounds to Kilograms");
-    printf("Enter Your choice (1/2): "); 
+    printf("2. Pounds to Kilograms\n");
+    printf("Enter Your Choice ( 1 or 2): ");
+    scanf("%d", &choice);
 
-    if (choice == 1)
-    {
+    // Conditions 
+    if(choice == 1) {
         // Kilograms to Pounds
-        scanf("%f", &choice);
-        printf("Enter the weight in Kilograms");
-        pounds = kilograms * 2.20462;
-        printf("%.2f kilograms is equal to %.2f Pounds", kilograms, pounds);
+        printf("Enter the weight in kilograms");
+    } 
+    else if (choice == 2)
+    {
+        // Pounds to Kilograms
+        printf("Enter the weight in Pounds");
     }
-    else if(choice == 2) {
-        printf("Enter the widht in pounds: ");
-        scanf("%f", &pounds);
-        kilograms = pounds * 2.20234;
-        printf("%.2f Pound is equal to %.2f Kilograms", pounds, kilograms);
-    }
-    else{
-        printf('Invalid choice! Please enter 1 or 2\n');
-    }
+    else {
+        printf("Invalid Choice! Please enter 1 or 2\n");
+    } 
     
 
     return 0;
