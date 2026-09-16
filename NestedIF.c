af5c5ca03a9d1675f680e877c1cb06cd66324532
FerdouseOfficial <bsource@gmail.com>
2026-09-16 07:57:28 +0600

    I think we are done with this

--- NestedIF.c
+++ NestedIF.c
@@ -4,11 +4,31 @@
 int main() {
 
     float price = 10.00;
-    bool isStudent = true;
+    bool isStudent = true; // 10% discount
+    bool isSenior = true; // 20% discount
+
+
+// student = $9;
+// Senior = $8;
+// Student + senior = $7;
+
 
     if(isStudent) {
-        printf("You get a studnet discount of 10%\n");
-        price *= 0.9;
+        if(isSenior) {
+            printf("You got a discount of 10%\n");
+            printf("You got a senior discount of 20%\n");
+            price *= 0.7;
+        }
+        else{
+            printf("You get a studne discount of 10%\n");
+            price *= 0.9;
+        }
+    }
+    else{
+        if(isSenior){
+            printf("You get a senior dicount of 20%\n");
+            price *= 0.8;
+        }
     }
 
     printf("The pricie of a ticket is: $%.2f\n", price);
